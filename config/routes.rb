a9676bb5d72110f859ded54fc9d2a3cc098d9aa4
root <root@localhost>
2022-09-17 04:16:40 +0000

    heroku pipeline setup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :authentications
   resources :accounts
   resources :statuses
+  root 'accounts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
