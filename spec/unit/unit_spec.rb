58035c0e11997c0b1b2d836fe6e8fe51e8858b75
RobertYam7 <robert8y@gmail.com>
2022-09-19 01:06:41 -0500

    added integration and unit tests for Status

--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Book, type: :model do
+RSpec.describe Status, type: :model do
   subject do
-    described_class.new(title: 'Harry Potter', author: 'JK Rowling', price: 19.99, published_date: '2020-09-28')
+    described_class.new(Value: 'Officer')
   end
 
   it 'is valid with all valid attributes' do
@@ -11,21 +11,6 @@ RSpec.describe Book, type: :model do
 
   it 'is not valid without a title' do
     subject.title = nil
-    expect(subject).not_to be_valid
-  end
-
-  it 'is not valid without a author' do
-    subject.author = nil
-    expect(subject).not_to be_valid
-  end
-
-  it 'is not valid without a price' do
-    subject.price = nil
-    expect(subject).not_to be_valid
-  end
-
-  it 'is not valid without a published_date' do
-    subject.published_date = nil
     expect(subject).not_to be_valid
   end
 end
