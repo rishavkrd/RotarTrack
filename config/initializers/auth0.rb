b1b014fac2924aa7216bbcab3abd63b4f445be4e
Ryan O'Malley <rd.omalley@comcast.net>
2022-09-19 22:40:19 -0500

    hotfix: dumb heroku

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,15 +1,16 @@
-# ./config/initializers/auth0.rb
-AUTH0_CONFIG = Rails.application.config_for(:auth0)
+# TODO: FIX THIS DISGUSTING BAND-AID FIX. SECRET KEY IS IN PLAINTEXT!!!!! 
+
+Rails.application.config.auth0 = Rails.application.config_for(:auth0)
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
-    AUTH0_CONFIG['auth0_client_id'],
-    AUTH0_CONFIG['auth0_client_secret'],
-    AUTH0_CONFIG['auth0_domain'],
+    'giksZR9ko8z8SrwI6Y8RolkR0SaySMJO',
+    'MUAmyw9XWkeIGk-rSUELQTbwDupvLVhLY3Zj-7R-UT1X8pG4irYqE6UqiaGkSZZ7',
+    'rotartrack.us.auth0.com',
     callback_path: '/auth/auth0/callback',
     authorize_params: {
-      scope: 'openid profile'
+      scope: 'openid profile email'
     }
   )
 end
