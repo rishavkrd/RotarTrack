d973efaea832f5be7b8e8f87508de90547823ba9
rishavkrd <rishavkrd11@tamu.edu>
2022-10-05 16:53:31 -0500

    dashboard organized keeping development buttons

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,8 @@ class DashboardController < ApplicationController
     include Secured
     def show
       @user = session[:userinfo]
+      @events = Event.all
+    end
+    def help
     end
   end
