a935b4d1cc4cd86803b83ba82a44a1713d1aa6e5
rishavkrd <rishavkrd11@tamu.edu>
2022-10-12 17:31:48 -0500

    1.dashboard organized, 2. view, edit my profile linked to the logged in user, 3. feature to create account when logged in user's email id is not in database added

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,17 @@ class DashboardController < ApplicationController
     def show
       @user = session[:userinfo]
       @events = Event.all
+      usr_email = @user["name"]
+      @account = Account.find_by_Email (usr_email)
+      # respond_to do |format|
+      if @account==nil
+        redirect_to new_account_path, notice: "New Account was successfully added." 
+        # format.json { render :show, status: :ok, location: @account }
+      # else
+      #   # format.html { redirect_to '/events', notice: "New Account was successfully added." }
+      #   format.json { render :show, status: :ok, location: @account }
+      end
+    # end
     end
     def help
     end
