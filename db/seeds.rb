9e1ce7296ef73e1690508ea13d8aed9f11bd4576
ckunah1 <ckunahl@gmail.com>
2022-10-15 13:46:51 -0500

    fixed the issue of creating account.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+Status.create!([
+  {Value: "officer"},
+  {Value: "Member"},
+  {Value: "Inactive"}
+])
 Account.create!([
   {UIN: 923874129, FirstName: "Joe", LastName: "Smith", PhoneNumber: 19283712, Email: "joe@smith.com", status_id: 2},
   {UIN: 344523451, FirstName: "Rishav", LastName: "Dokania", PhoneNumber: 129883712, Email: "rishavkrd11@tamu.edu", status_id: 1},
@@ -6,18 +11,13 @@ Account.create!([
   {UIN: 1231412, FirstName: "Henry", LastName: "Ford", PhoneNumber: 198263129, Email: "henry@ford.com", status_id: 2},
   {UIN: 431414124, FirstName: "Tom", LastName: "Hanks", PhoneNumber: 121421124, Email: "tom@tu.edu", status_id: 2}
 ])
-Event.create!([
-  {Title: "Meetup with team", Description: "Meet the officers and members of AggieRotaract", Date: "2022-12-12", Time: "2000-01-01 21:45:00", Location: "MSC", Points: 1, type_id: 1}
-])
-Status.create!([
-  {Value: "officer"},
-  {Value: "Member"},
-  {Value: "Inactive"}
-])
 Type.create!([
   {Value: "Meeting"},
   {Value: "Event"},
   {Value: "Social"},
   {Value: "Other"},
   {Value: "Inactive"}
+])
+Event.create!([
+  {Title: "Meetup with team", Description: "Meet the officers and members of AggieRotaract", Date: "2022-12-12", Time: "2000-01-01 21:45:00", Location: "MSC", Points: 1, type_id: 1}
 ])
