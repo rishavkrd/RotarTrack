e4beee18de64550281a94efeb1160cc49e6616d3
rdcrowell13 <rdcrowell13@gmail.com>
2022-10-17 12:41:16 -0500

    signup model no longer validates pickup. It's now optional

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,8 @@ Account.create!([
   { UIN: 812_658_276, FirstName: 'Sam', LastName: 'McDonalds', PhoneNumber: 12_398_127, Email: 'sam@tamu.edu', status_id: 2 },
   { UIN: 9_818_273, FirstName: 'John', LastName: 'Goodman', PhoneNumber: 1_928_371, Email: 'john@tamu.edu', status_id: 2 },
   { UIN: 1_231_412, FirstName: 'Henry', LastName: 'Ford', PhoneNumber: 198_263_129, Email: 'henry@ford.com', status_id: 2 },
-  { UIN: 431_414_124, FirstName: 'Tom', LastName: 'Hanks', PhoneNumber: 121_421_124, Email: 'tom@tu.edu', status_id: 2 }
+  { UIN: 431_414_124, FirstName: 'Tom', LastName: 'Hanks', PhoneNumber: 121_421_124, Email: 'tom@tu.edu', status_id: 2 },
+  { UIN: 927_003_214, FirstName: 'Ryan', LastName: 'Crowell', PhoneNumber: 121_421_124, Email: 'rdcrowell13@tamu.edu', status_id: 2}
 ]
                )
 Type.create!([
