cfe7519bac49b5b5cdd5605b50c7d5204d60286e
rishavkrd <rishavkrd11@tamu.edu>
2022-10-26 16:49:41 -0500

    1. Added 'Points' attribute in points entity, 2. added total points in the my accounts view page, 3. fixed UIN type to string 4. Added list of log of points in the profile

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,6 +7,7 @@ class DashboardController < ApplicationController
     @events = Event.all
     usr_email = @user['email']
     @account = Account.find_by(Email: usr_email)
+    
     # respond_to do |format|
     # if @account==nil
     #   redirect_to new_account_path, notice: "New Account was successfully added."
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_18_021932) do
+ActiveRecord::Schema.define(version: 2022_10_26_212203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
-    t.integer "UIN"
+    t.text "UIN"
     t.text "FirstName"
     t.text "LastName"
     t.text "PhoneNumber"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 2022_10_18_021932) do
     t.bigint "event_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "Points"
     t.index ["account_id"], name: "index_points_on_account_id"
     t.index ["event_id"], name: "index_points_on_event_id"
   end
