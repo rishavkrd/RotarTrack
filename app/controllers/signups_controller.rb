e5415f17927cb18669d1febffbb431eff49cb464
RobertYam7 <robert8y@gmail.com>
2022-10-31 22:43:33 -0500

    implemented 6 hour cutoff and event details stories

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -25,17 +25,28 @@ class SignupsController < ApplicationController
     @signup = Signup.new(signup_params)
     eventDateTime = DateTime.new(@signup.event.Date.year, @signup.event.Date.month, @signup.event.Date.day, @signup.event.Time.hour, @signup.event.Time.min, @signup.event.Time.sec)
     cutoffDateTime = eventDateTime - (6/24.0)
-    respond_to do |format|
-      if (DateTime.now.in_time_zone("Central Time (US & Canada)") > cutoffDateTime)
-        format.html { render(:new, notice: "Cannot signup within 6 hours of event start time.") }
-        format.json { render(json: @signup.errors, status: :unprocessable_entity) }
+    p eventDateTime
+    p cutoffDateTime
+    p DateTime.now.in_time_zone("Central Time (US & Canada)")
+    signupTime = DateTime.now.in_time_zone("Central Time (US & Canada)")
+    convertedTime = DateTime.new(signupTime.year, signupTime.month, signupTime.day, signupTime.hour, signupTime.min, signupTime.sec)
+    p convertedTime
+    if (convertedTime > cutoffDateTime)
+      respond_to do |format|
+        p "current time is over cutoff"
+        format.html { redirect_to({ :action => "new"}, notice: "Cannot sign up for event within 6 hours of start time") }
+        format.json { render :new, status: :unprocessable_entity, location: @signup }
       end
-      if (@signup.save)
-        format.html { redirect_to signup_url(@signup), notice: "Signup was successfully created." }
-        format.json { render :show, status: :created, location: @signup }
-      else
-        format.html { render(:new, status: :unprocessable_entity) }
-        format.json { render(json: @signup.errors, status: :unprocessable_entity) }
+    else
+      respond_to do |format|
+        if (@signup.save)
+          p "signup created"
+          format.html { redirect_to signup_url(@signup), notice: "Signup was successfully created." }
+          format.json { render :show, status: :created, location: @signup }
+        else
+          format.html { render(:new, status: :unprocessable_entity) }
+          format.json { render(json: @signup.errors, status: :unprocessable_entity) }
+        end
       end
     end
   end
@@ -44,17 +55,27 @@ class SignupsController < ApplicationController
   def update
     eventDateTime = DateTime.new(@signup.event.Date.year, @signup.event.Date.month, @signup.event.Date.day, @signup.event.Time.hour, @signup.event.Time.min, @signup.event.Time.sec)
     cutoffDateTime = eventDateTime - (6/24.0)
-    respond_to do |format|
-      if (DateTime.now.in_time_zone("Central Time (US & Canada)") > cutoffDateTime)
-        format.html { render(:new, notice: "Cannot signup within 6 hours of event start time.") }
-        format.json { render(json: @signup.errors, status: :unprocessable_entity) }
+    p eventDateTime
+    p cutoffDateTime
+    p DateTime.now.in_time_zone("Central Time (US & Canada)")
+    signupTime = DateTime.now.in_time_zone("Central Time (US & Canada)")
+    convertedTime = DateTime.new(signupTime.year, signupTime.month, signupTime.day, signupTime.hour, signupTime.min, signupTime.sec)
+    p convertedTime
+    if (convertedTime > cutoffDateTime)
+      respond_to do |format|
+        format.html { redirect_to({ :action => "edit"}, notice: "Cannot sign up for event within 6 hours of start time") }
+        format.json { render :edit, status: :unprocessable_entity, location: @signup }
       end
-      if @signup.update(signup_params)
-        format.html { redirect_to(signup_url(@signup), notice: 'Signup was successfully updated.') }
-        format.json { render(:show, status: :ok, location: @signup) }
-      else
-        format.html { render(:edit, status: :unprocessable_entity) }
-        format.json { render(json: @signup.errors, status: :unprocessable_entity) }
+    else
+      respond_to do |format|
+        if (@signup.save)
+          p "signup created"
+          format.html { redirect_to signup_url(@signup), notice: "Signup was successfully created." }
+          format.json { render :show, status: :created, location: @signup }
+        else
+          format.html { render(:edit, status: :unprocessable_entity) }
+          format.json { render(json: @signup.errors, status: :unprocessable_entity) }
+        end
       end
     end
   end
