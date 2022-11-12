07b36bba5d6f131f6d545a08ed0078aa842fb2a7
Ryan O'Malley <rd.omalley@comcast.net>
2022-11-12 16:27:16 -0600

    ui: homepage complete. profile creation complete. dashboard in prog.

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -10,17 +10,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'maroon': { 
-          '50': '#FF1A12', 
-          '100': '#FF0D05', 
-          '200': '#EB0600', 
-          '300': '#D10400', 
-          '400': '#B80300', 
-          '500': '#9E0200', 
-          '600': '#850100', 
-          '700': '#6B0000', 
+        'maroon': {
+          '50': '#FF1A12',
+          '100': '#FF0D05',
+          '200': '#EB0600',
+          '300': '#D10400',
+          '400': '#B80300',
+          '500': '#9E0200',
+          '600': '#850100',
+          '700': '#6B0000',
           '800': '#570000',
-          '900': '#420001' 
+          '900': '#420001'
         }
       },
       fontFamily: {
@@ -28,9 +28,36 @@ module.exports = {
       },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        aggie: {
+
+          "primary": "#6B0000",
+
+          "secondary": "#003C71",
+
+          "accent": "#FCE300",
+
+          "neutral": "#3D4451",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#3ed488",
+
+          "warning": "#FBBD23",
+
+          "error": "#E4002B",
+        },
+      },
+    ],
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require("daisyui"),
   ]
 }
