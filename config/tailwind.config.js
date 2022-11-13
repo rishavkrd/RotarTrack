afdbffe5112a9efb3f5fd8461685135f68494a57
Ryan O'Malley <rd.omalley@comcast.net>
2022-11-13 14:03:26 -0600

    yarn add daisyui

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -58,6 +58,6 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-    require('node_modules/daisyui'),
+    require('daisyui'),
   ]
 }
