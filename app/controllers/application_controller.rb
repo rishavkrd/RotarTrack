1363c119debbcb937e7a865496e2a448f7f472d2
Ryan O'Malley <rd.omalley@comcast.net>
2022-11-13 19:58:12 -0600

    progress checkpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,8 @@ class ApplicationController < ActionController::Base
     def get_user_email
       @user = session[:userinfo]
       email = @user["email"]
+      if $current_user.nil?
+        $current_user = Account.find_by uuid: session[:useruuid]
+      end
     end
 end
