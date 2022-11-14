7a3873bbecd8d746ccf8c448cfdbce7a79bd6402
Ryan O'Malley <rd.omalley@comcast.net>
2022-11-13 21:42:24 -0600

    User index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,5 @@ class ApplicationController < ActionController::Base
     def get_user_email
       @user = session[:userinfo]
       email = @user["email"]
-      if $current_user.nil?
-        $current_user = Account.find_by uuid: session[:useruuid]
-      end
     end
 end
